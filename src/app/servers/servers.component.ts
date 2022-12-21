b09d588453100bed408ece9240702de512a95175
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2022-12-21 20:59:21 +0530

    Html Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
    selector: 'app-servers',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
 
+allowNewServer : boolean = false;
+
+constructor() {
+  
+setTimeout(() => {
+  this.allowNewServer =true;
+}, 2000);
+}
+
+ngOnInit(){
+  throw new Error('Method not implemented.');
+}
 }
