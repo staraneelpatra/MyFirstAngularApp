f99b0e60add028a033368008ee85f999c1e0639f
staraneelpatra <staraneel@live.com>
2022-12-22 09:59:25 +0530

    Directive ngIf and Else and Used.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ export class ServersComponent implements OnInit {
 allowNewServer : boolean = false;
 newServerCreated : string = "No new Server Created...";
 serverName : string = '';
-
+isServerCreated :  boolean = false;
 constructor() {  
 setTimeout(() => {
   this.allowNewServer =true;
@@ -23,6 +23,7 @@ ngOnInit(){
   throw new Error('Method not implemented.');
 }
 CreateNewServer(){
+  this.isServerCreated = true;
   this.newServerCreated = "New Server Created ! Server Name is " + this.serverName;
 }
 }
