4c5f4d2447fa65ece319e608927856932f0baab8
staraneelpatra <staraneel@live.com>
2022-12-22 21:12:49 +0530

    ngClass Directive assigns Style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'app-server',
-    templateUrl : './server.component.html'
+    templateUrl : './server.component.html',
+    styles : [`.Online{color:white}`]
 })
 export class ServerComponent{
     serverid : number = 10;
